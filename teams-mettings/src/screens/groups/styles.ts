b870f6246b690f02de2a2f604fb0324d4e268f65
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-04-08 13:10:40 -0300

    refactor: Remove unused Title styled component from group screen styles

--- teams-mettings/src/screens/groups/styles.ts
+++ teams-mettings/src/screens/groups/styles.ts
@@ -6,9 +6,3 @@ export const Container = styled.View`
   align-items: center;
   justify-content: center;
 `;
-
-export const Title = styled.Text`
-  font-size: ${({ theme }) => theme.FONT_SIZE.XL}
-  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
-  color: ${({ theme }) => theme.COLORS.WHITE}; 
-`;
