1cb5fa6e0342bea1bf3c0aebbfe1cbe4140a0321
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-04-08 13:11:54 -0300

    fix: Correctly format Title styled component in group screen styles

--- teams-mettings/src/screens/groups/styles.ts
+++ teams-mettings/src/screens/groups/styles.ts
@@ -6,3 +6,9 @@ export const Container = styled.View`
   align-items: center;
   justify-content: center;
 `;
+
+export const Title = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL};
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  color: ${({ theme }) => theme.COLORS.WHITE}; 
+`;
