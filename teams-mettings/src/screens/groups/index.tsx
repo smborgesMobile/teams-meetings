b76358255697455311be5aec1e4a3da6888705d8
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-04-24 11:17:02 -0300

    feat: replace static GroupCard with FlatList for dynamic group rendering

--- teams-mettings/src/screens/groups/index.tsx
+++ teams-mettings/src/screens/groups/index.tsx
@@ -2,17 +2,30 @@ import { Header } from "@/src/components/header";
 import { HightLight } from "@/src/components/hightlight";
 import { GroupCard } from "@/src/components/groupcard";
 import { Container } from "./styles"
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>([
+        "Rocket Seat",
+        "Galera do Ignite",
+        "Turma do React Native"]);
+
     return (
         <Container>
             <Header showBackButton={false} />
             <HightLight
                 title="Turmas"
                 subtitle="Jogue com sua turma" />
-            <GroupCard
-                onPress={() => { console.log("Grupo 1") }}
-                title="Galera do Ignite" />
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                        onPress={() => { console.log(item) }} />
+                )}
+            />
         </Container>
     );
 }
